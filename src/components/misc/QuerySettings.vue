ef9d6fafee3aa3bd7b98f1b206f12faec6e89b7e
LevKhaak <leu.khaak@gmail.com>
2023-03-30 08:08:14 +0300

    error with lang=scss&scoped=true

--- src/components/misc/QuerySettings.vue
+++ src/components/misc/QuerySettings.vue
@@ -1,44 +1,57 @@
+<!-- eslint-disable no-tabs -->
+<!-- eslint-disable vue/script-indent -->
+<!-- eslint-disable vue/script-indent -->
+<!-- eslint-disable no-tabs -->
+<!-- eslint-disable vue/html-indent -->
+<!-- eslint-disable no-tabs -->
 <template>
-  <div class="query-settings">
-    Query-settings
-    <select size="4" name="max_tokens" @input="show()" v-model="tokens">
+    <div class="query-settings">
+        <h3>Query-settings</h3>
+        <br />
+        <input
+            type="number"
+            name="max_tokens"
+            min="50"
+            max="1000"
+            placeholder="200"
+            value="200"
+            @input="$emit('update:value', newOpts), show()"
+        />
+        <!--<select size="4" name="max_tokens" @input="show()" v-model="tokens">
       <option disabled>max_tokens</option>
       <option value="10">10</option>
       <option value="200">200</option>
       <option value="500">500</option>
       <option value="1000">1000</option>
-    </select>
-  </div>
+    </select>-->
+    </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { defineComponent, PropType, ref } from "vue";
+import { CreateCompletionRequest } from "openai";
 
 export default defineComponent({
-  props: {
-    opts: Object,
-    changeMaxTokens: Function,
-  },
-  components: {},
-  data() {
-    return {
-      tokens: "300",
-      value: String,
-    };
-  },
-  computed: {},
-  methods: {
-    show() {
-      console.log("CHILD", this.tokens);
-      if (this.changeMaxTokens) this.changeMaxTokens(+this.tokens);
+    props: {
+        value: Object as PropType<Partial<CreateCompletionRequest>>,
+        changeMaxTokens: Function,
     },
-  },
-  // mounted() {},
+    components: {},
+    data() {
+        return {};
+    },
+    computed: {},
+    methods: {
+        show() {
+            if (this.changeMaxTokens) this.changeMaxTokens();
+        },
+    },
+    // mounted() {},
 });
 </script>
 
 <style lang="scss" scoped>
 .query-settings {
-  color: #000;
+    color: #000;
 }
 </style>
