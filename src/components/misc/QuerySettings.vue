358d1675db9a741c124fa8228d31bb947fe30a63
LevKhaak <leu.khaak@gmail.com>
2023-03-30 20:47:32 +0300

    error with scoped solved

---
+++ src/components/misc/QuerySettings.vue
@@ -0,0 +1,50 @@
+<!-- eslint-disable no-tabs -->
+<!-- eslint-disable vue/script-indent -->
+<!-- eslint-disable vue/script-indent -->
+<!-- eslint-disable no-tabs -->
+<!-- eslint-disable vue/html-indent -->
+<!-- eslint-disable no-tabs -->
+<template>
+    <div class="query-settings">
+        <h3>Query-settings</h3>
+        <br />
+        <input
+            type="number"
+            name="max_tokens"
+            min="50"
+            max="1000"
+            placeholder="200"
+            value="200"
+			@input="$emit('update:value', newOpts), show()"          
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from "vue";
+import { CreateCompletionRequest } from "openai";
+
+export default defineComponent({
+    props: {
+        value: Number, // Object as PropType<Partial<CreateCompletionRequest>>,
+        changeMaxTokens: Function,
+    },
+    components: {},
+    data() {
+        return {};
+    },
+    computed: {},
+    methods: {
+        show() {
+            if (this.changeMaxTokens) this.changeMaxTokens();
+        },
+    },
+    // mounted() {},
+});
+</script>
+
+<style lang="scss" scoped>
+.query-settings {
+    color: #000;
+}
+</style>
