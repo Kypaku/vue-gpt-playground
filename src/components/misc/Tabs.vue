deade868aa0a5332a89a9d5b63e57b00b932fbc8
LevKhaak <leu.khaak@gmail.com>
2023-05-03 21:40:04 +0300

    tab style changed-2

--- src/components/misc/Tabs.vue
+++ src/components/misc/Tabs.vue
@@ -1,9 +1,12 @@
 <template>
     <ul class="tabs flex items-center">
         <li
-            class="tab px-2 py-1 cursor-pointer"
-            :class="{active: value ? (value === tab.value) : i === 0}"
-            v-for="(tab, i) in tabs" :key="i"
+            class="tab px-4 py-2 cursor-pointer flex-grow text-center bg-gray-50 hover:bg-gray-200"
+            :class="{
+                'active bg-gray-200': value ? value === tab.value : i === 0,
+            }"
+            v-for="(tab, i) in tabs"
+            :key="i"
             @click="$emit('update:value', tab.value)"
         >
             <span>{{ tab.label }}</span>
@@ -11,46 +14,32 @@
     </ul>
 </template>
 
-<script lang='ts'>
-    import { defineComponent, PropType } from "vue"
-    ;
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
 
-    export interface ITab {
-        label: string
-        value: string
-    }
+export interface ITab {
+    label: string;
+    value: string;
+}
 
-    export default defineComponent({
-        props: {
-            tabs: Object as PropType<ITab[]>,
-            value: String,
+export default defineComponent({
+    props: {
+        tabs: Object as PropType<ITab[]>,
+        value: String,
+    },
+    components: {},
+    data() {
+        return {};
+    },
+    computed: {},
+    methods: {},
 
-        },
-        components: {
-
-        },
-        data() {
-            return {
-
-            };
-        },
-        computed: {
-
-        },
-        methods: {
-
-        },
-
-        watch: {
-
-        },
-    });
-
-    </script>
+    watch: {},
+});
+</script>
 
 <style lang="scss" scoped>
-    .active{
-        border-bottom: 2px solid rgb(102, 102, 102);
-    }
-
+.active {
+    border-bottom: 2px solid rgb(102, 102, 102);
+}
 </style>
